ccfee8b7ad6e51651ebd1c863f39cf3d14bbb435
Erastus11013 <erastuswagwan@gmail.com>
2019-12-12 10:38:50 -0500

    Update treap.py

--- treap.py
+++ treap.py
@@ -1,193 +1,268 @@
 import numpy as np
-from pprint import pprint
+from random import randint
+from math import inf, log2
+from datetime import datetime
 
 
-class CartesianTree:
+class RMQ:
+    """Range Minimum Query class
+    Has three methods to construct it
+    construct seg_tree is so far the fastest
+    @param: a: an array of integers, floats, or complex numbers"""
+
     def __init__(self, a):
-        self.array = np.array(a)
-        self.root = self.right = self.left = self.parent = None
+        self.array = a
+        self.M = []
+        self.algo_used = 't'
 
-    def build_cartesian_tree(self):
-        """I use arrays for left, right, parent because array indexing is faster than attribute access"""
-        if len(self.array) < 1:
-            raise ValueError("cannot build cartesian tree from empty array")
+    def construct_naive_rmq(self, array=None, lookup=None):
+        """Trivial algorithm for RMQ
+        For every pair of indices (i, j) store the value of RMQ(i, j) in a table M[0, N-1][0, N-1].
+        Using an easy dynamic programming approach we can reduce the complexity to <O(N^2), O(1)>
+        Uses O(N^2) space"""
         n = len(self.array)
-        self.right = np.zeros(n, dtype='int32')
-        self.left = np.zeros(n, dtype='int32')
-        self.parent = np.zeros(n, dtype='int32')
+        if array is None:
+            array = self.array
+        if lookup is None:
+            lookup = self.M = np.full((n, n), inf, dtype='int32')
 
-        self.root = 0
-        self.left[0] = -1
-        self.right[0] = -1
-        self.parent[0] = -1
+        self.algo_used = 'n'
+        for i in range(n):   # O(n)
+            self.M[i][i] = i
 
-        for i in range(1, n):
-            last = i - 1
-            self.right[i] = -1
+        for i in range(n):
+            for j in range(i + 1, n):
+                if array[self.M[i][j - 1]] < array[j]:
+                    lookup[i][j] = self.M[i][j-1]
+                else:
+                    lookup[i][j] = j
 
-            while self.array[last] > self.array[i] and last != self.root:
-                last = self.parent[last]
+    def construct_sparse_table(self, array=None, sparse=None):
+        """preprocess RMQ for sub arrays of length 2k using dynamic programming.
+        We will keep an array M[0, N-1][0, logN]
+        where M[i][j] is the index of the minimum value in the sub array starting at i having length 2^j.
+        So, the overall complexity of the algorithm is <O(N logN), O(1)>
+        Uses O(N logN) space"""
 
-            if self.array[last] > self.array[i]:
-                self.parent[self.root] = i
-                self.left[i] = self.root
-                self.root = i
-
-            elif self.right[last] == -1:
-                self.right[last] = i
-                self.parent[i] = last
-                self.left[i] = -1
-            else:
-                self.parent[self.right[last]] = i
-                self.left[i] = self.right[last]
-                self.right[last] = i
-                self.parent[i] = last
-
-    def in_order(self, start=None):
-        """
-        In order traversal of a tree can be described in three simple steps:
-            1. traverse the left subtree
-            2. visit root
-            3. traverse the right subtree.
-
-        Args:
-            start: the current node.
-
-        Returns:
-            an array of the nodes.
-        """
-
-        def helper(current, array):
-            if self.left[current] == -1 and self.right[current] == -1:
-                array.append(self.array[current])
-            else:
-                if self.left[current] != -1:
-                    helper(self.left[current], array)
-                array.append(self.array[current])
-                if self.right[current] != -1:
-                    helper(self.right[current], array)
-
-        if start is None:
-            start = self.root
-        args = []
-        helper(start, args)
-        return np.array(args)
-
-    def post_order(self, start=None):
-
-        # noinspection DuplicatedCode
-        def helper(current, array):
-            if self.left[current] == -1 and self.right[current] == -1:
-                array.append(self.array[current])
-            else:
-                if self.left[current] != -1:
-                    helper(self.left[current], array)
-                if self.right[current] != -1:
-                    helper(self.right[current], array)
-                array.append(self.array[current])
-
-        if start is None:
-            start = self.root
-        args = []
-        helper(start, args)
-        return np.array(args)
-
-    def pre_order(self, start=None):
-
-        # noinspection DuplicatedCode
-        def helper(current, array):
-            if self.left[current] == -1 and self.right[current] == -1:
-                array.append(self.array[current])
-            else:
-                array.append(self.array[current])
-                if self.left[current] != -1:
-                    helper(self.left[current], array)
-                if self.right[current] != -1:
-                    helper(self.right[current], array)
-
-        if start is None:
-            start = self.root
-        args = []
-        helper(start, args)
-        return np.array(args)
-
-    def euler_tour(self, source=None):
-        """Works for DAGS"""
-        args = []
-
-        def euler_visit(current, order):
-            order.append(self.array[current])
-            if self.left[current] == -1 and self.right[current] == -1:
-                return
-            else:
-                if self.left[current] != -1:
-                    euler_visit(self.left[current], order)
-                    order.append(self.array[current])
-                if self.right[current] != -1:
-                    euler_visit(self.right[current], order)
-                    order.append(self.array[current])
-
-        if source is None:
-            source = self.root
-        euler_visit(source, args)
-        print('->'.join(map(str, args)))
-        return np.array(args)
-
-
-class CartesianTreeRMQ(CartesianTree):
-    def __init__(self, a):
-        super().__init__(a)
-
-    def euler_tour(self, source=None):
+        self.algo_used = 'st'
         n = len(self.array)
-        E = []
-        D = []
-        R = np.full(n, -1, dtype='int32')
-        if self.root is None:
-            raise ValueError("Build cartesian tree first")
+        m = int(log2(n))
+        if sparse is None:
+            sparse = np.full((n, m + 1), inf, dtype='int32')
+            self.M = sparse
+        if array is None:
+            array = self.array
 
-        def euler_visit(current, labels, depths, rep, curr_depth, index):
-            depths.append(curr_depth)
-            labels.append(self.array[current])
-            if rep[current] == -1:
-                rep[current] = index
-            if not (self.left[current] == -1 and self.right[current] == -1):
-                if self.left[current] != -1:
-                    euler_visit(self.left[current], labels, depths, rep, curr_depth + 1, index + 1)
-                    labels.append(self.array[current])
-                    depths.append(curr_depth)
-                    index += 1
-                if self.right[current] != -1:
-                    euler_visit(self.right[current], labels, depths, rep, curr_depth + 1, index + 1)
-                    labels.append(self.array[current])
-                    depths.append(curr_depth)
-                    index += 1
+        for i in range(n):  # intervals of length 1
+            sparse[i][0] = i
+        for j in range(1, m + 1):  # log(n)
+            if (1 << j) > n:  # 1 << j == 2^j
+                break
+            for i in range(n):
+                if (i + (1 << j) - 1) >= n:
+                    break  # i + 2^j - 1
+                else:
+                    if array[sparse[i][j - 1]] < array[sparse[i + (1 << (j - 1))][j - 1]]:
+                        sparse[i][j] = sparse[i][j - 1]
+                    else:
+                        sparse[i][j] = sparse[i + (1 << (j - 1))][j - 1]
 
-        if source is None:
-            source = self.root
-        euler_visit(source, E, D, R, 0, 0)
-        # print('->'.join(map(str, E)))
-        return np.array(E), np.array(D), np.array(R)
+    def construct_rmq_segment_tree(self):
+        """A segment tree or segtree is a basically a binary tree used for storing the intervals or segments.
+        Each node in the segment tree represents an interval.
+        Consider an array A of size N and a corresponding segtree T:
+        The root of T will represent the whole array A[0:N-1].
+        Each leaf in the segtree T will represent a single element A[i] such that 0 <= i < N.
+        The internal nodes in the segtree tree T represent union of elementary intervals A[i:j] where 0 <= i < j < N.
+        The root of the segtree will represent the whole array A[0:N-1].
+        Then we will break the interval or segment into half and the two children of the root will represent the
+        A[0:(N-1) / 2] and A[(N-1) / 2 + 1:(N-1)].
+        So in each step we will divide the interval into half and the two children will represent the two halves.
+        So the height of the segment tree will be log2N.
+        There are N leaves representing the N elements of the array.
+        The number of internal nodes is N-1. So total number of nodes are
+        O(N) space: Array of size 2N."""
+
+        self.algo_used = 't'
+        n = len(self.array)
+        max_size = n << 1 + 1
+        self.M = np.array([0] * max_size)
+        self._initialize(0, 0, n-1)
+
+    def construct_rmq_sqrt(self):
+        """An interesting idea is to split the vector in sqrt(N) pieces.
+        We will keep in a vector M[0, sqrt(N)-1] the position for the minimum value for each section.
+        M can be easily preprocessed in O(N)"""
+
+    def _query_sparse_table(self, low, high, array=None, sparse=None):
+        """In this operation we can query on an interval or segment and
+         return the answer to the problem on that particular interval."""
+
+        if array is None:
+            array = self.array
+        if sparse is None:
+            sparse = self.M
+
+        length = (high - low) + 1
+        k = int(log2(length))
+        if array[sparse[low][k]] <= array[sparse[low + length - (1 << k)][k]]:
+            return sparse[low][k]
+        else:
+            return sparse[high - (1 << k) + 1][k]
+
+    def _initialize(self, current, low, high):
+        """Helper method to construct the segment tree"""
+        if low == high:  # we are at a leaf
+            self.M[current] = low
+        else:
+            mid = (low + high) >> 1
+
+            left = current * 2 + 1
+            right = current * 2 + 2
+            self._initialize(left, low, mid)
+            self._initialize(right, mid + 1, high)
+            if self.array[self.M[left]] <= self.array[self.M[right]]:
+                self.M[current] = self.M[left]
+            else:
+                self.M[current] = self.M[right]
+
+    def _query_segment_tree(self, curr, low, high, i, j):
+        """To query on a given range, we need to check 3 conditions:
+            range represented by a node is completely inside the given range
+            range represented by a node is completely outside the given range
+            range represented by a node is partially inside and partially outside the given range"""
+
+        # range represented by a node is completely outside the given range
+        if i > high or j < low:
+            return -1
+
+        # if the current interval is included in the query interval return M[curr]
+        if i <= low and high <= j:
+            return self.M[curr]
+
+        #  compute arg_min in the left and right interval
+        mid = (low + high) >> 1
+        p1 = self._query_segment_tree(2 * curr + 1, low, mid, i, j)
+        p2 = self._query_segment_tree(2 * curr + 2, mid + 1, high, i, j)
+
+        # find and return arg_min(self.A[i: j])// return the
+        if p2 == -1:
+            return p1
+        if p1 == -1:
+            return p2
+        if self.array[p1] <= self.array[p2]:
+            return p1
+        else:
+            return p2
+
+    def rmq(self, i, j):
+        """Return the argmin in the range [i:j] of the array"""
+        if self.algo_used == 'n':
+            return self.M[i][j]
+        elif self.algo_used == 'st':  # sparse table algorithm
+            return self._query_sparse_table(i, j)
+        elif self.algo_used == 't':
+            return self._query_segment_tree(0, 0, len(self.array) - 1, i, j)
+
+
+
+    @property
+    def algorithm_used(self):
+        if self.algo_used == 't':
+            return 'Segment tree'
+        elif self.algo_used == 'st':
+            return 'Sparse table algorithm'
+        elif self.algo_used == 'n':
+            return 'Naive dynamic programming algorithm'
+
+    def __getitem__(self, item):
+        """Assumes item is a slice object
+        returns the argmin of the array in range[start: stop]
+        Note that stop is included in the interval
+        """
+        n = len(self.array)
+        stop = item.stop
+        if stop is None or item.stop > n - 1:
+            stop = n - 1
+        else:
+            if stop < 0:
+                stop += n
+        assert isinstance(item, slice), print(item)
+        start = 0 if item.start is None else item.start
+
+        if start > stop:
+            raise IndexError("make sure start <= stop")
+        return self.rmq(start, stop)
+
+    def __repr__(self):
+        """Simple repr of the array in the RMQ class"""
+        if len(self.array) > 10:
+            return 'RMQ [' + '  '.join(map(str, self.array[: 10])) + ' ... ' + str(self.array[-2]) + ']'
+        else:
+            return 'RMQ [' + '  '.join(map(str, self.array)) + ']'
+
+
+# tests
+def test_naive_rmq_construct_method(test, i, j):
+    """test the naive method of constructing a range minimum query class
+        @param: test: the array to be used in the construction of the range minimum query
+        @param: i: the start of the interval
+        @param: j: the end of the interval
+        @return: a tuple of (argmin, min)"""
+    r = RMQ(test)
+    r.construct_naive_rmq()
+    x = r[i: j]
+    return x, r.array[x]
+
+
+def test_sparse_table_algorithm_method(test, i, j):
+    """test the sparse table algorithm for constructing a range minimum query class"""
+    r = RMQ(test)
+    r.construct_sparse_table()
+    x = r[i: j]
+    return x, r.array[x]
+
+
+def test_segment_tree_construction_method(test, i, j):
+    """test the segment tree construction of a range minimum query"""
+    r = RMQ(test)
+    r.construct_rmq_segment_tree()
+    x = r[i: j]
+    return x, r.array[x]
+
+
+def tests(size, fl, cap, n):
+    """Testing
+        @param: size: size of the random array to be constructed
+        @param: fl: the minimum value of randint
+        @param cap: the maximum value of randint
+        @param: n: the number of times to run the tests"""
+    for i in range(n):
+        # i = randint(0, size - 1)
+        # j = randint(i, size - 1)
+        #
+        # test = [randint(fl, cap) for _ in range(size)]]
+        i = 0
+        j = 15
+        # test = [0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0]
+        test = [20, 19, 18, 17, 18]
+
+        t1 = datetime.now()
+        arg_min1, minimum1 = test_naive_rmq_construct_method(test, i, j)
+        print("min using naive rmq: ", minimum1, "ran in time", (datetime.now() - t1).total_seconds(), "seconds...")
+
+        t2 = datetime.now()
+        arg_min2, minimum2 = test_sparse_table_algorithm_method(test, i, j)
+        print("min using sparse rmq: ", minimum2, "ran in time", (datetime.now() - t2).total_seconds(), "seconds...")
+        t3 = datetime.now()
+
+        arg_min3, minimum3 = test_segment_tree_construction_method(test, i, j)
+        print("min using seg tree: ", minimum3, "ran in time", (datetime.now() - t3).total_seconds(), "seconds...")
+        mininum4 = min(test[i:j + 1])
+        assert minimum1 == mininum4 == minimum2 == minimum3
+
+        print(arg_min1, arg_min2, arg_min3)
 
 
 if __name__ == '__main__':
-    from datetime import datetime
-    from random import randint
-
-    test = [9, 3, 7, 1, 8, 12, 10, 20, 15, 18, 5]
-    # test = [randint(0, 500) for _ in range(3000)]
-    tic = datetime.now()
-    c = CartesianTree(test)
-    c.build_cartesian_tree()
-    toc = datetime.now()
-    print("cartesian tree built in", (toc - tic).total_seconds(), 'seconds')
-
-    cr = CartesianTreeRMQ(test)
-    cr.build_cartesian_tree()
-    e, d, r = cr.euler_tour()
-    pprint(cr.array)
-    pprint(e)
-    pprint(d)
-    pprint(r)
-
-    # assert (len(c.euler_tour()) == len(c.array) * 2 - 1)
+    tests(1000, 13, 200, 1)
