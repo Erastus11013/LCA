9a9bcc1ab1e2b3f285c45908b8473c572cd4efaf
Erastus11013 <erastuswagwan@gmail.com>
2019-12-12 09:34:48 -0500

    Update restricted_rmq.py

--- restricted_rmq.py
+++ restricted_rmq.py
@@ -2,97 +2,165 @@ from rmq import RMQ
 from treap import CartesianTreeRMQ
 import numpy as np
 from math import log2, ceil, inf
+from sys import maxsize
 from copy import deepcopy
-import bitty
 
 
 class RestrictedRMQ(RMQ):
+    INFINITY = maxsize
 
     def __init__(self, a):
-        super().__init__(a)
-        self.E = None
-        self.depths = None
-        self.R = None
-        self.block_count = 0
-        self.block_size = 0
-        self.block_comb = 0
-        self.lca = True
-        self.sparse = None
-        self._build_cartesian_tree()
+        super().__init__(a)               # ------------- #
+        self.E = None                     # 2n - 1
+        self.depths = None                # 2n - 1
+        self.R = None                     # n
+        self.A = None                     # n
+        self.B = None                     # n
+        self.T = None                     # n
+        self.lookup = None                # sqrt(n) * 2n/lg n  #  o(n)
+        self.block_count = 0              # 1
+        self.block_size = 0               # 1
+        self.max_lookup = 0               # 1
+        self.lca = True                   # 1
+        self.sparse = None                # 2n/ lg n * log ( 2n/ lg n )
+        self._build_cartesian_tree()      # 3n ... but is deleted
 
     def _build_cartesian_tree(self):
         c = CartesianTreeRMQ(self.array)
         c.build_cartesian_tree()  # step 1
         self.E, self.depths, self.R = c.euler_tour()  # step 2
 
+    @staticmethod
+    def get_block_type(block, blocksize) -> int:
+        """To determine the type of block which x has, then you need to consider elements
+        in the range[0 ... n] if using 1-based indexing. For example if the blocksize is 4, then block must include the
+        last element of the previous block, for the first block, send in an array
+        """
+        bit = 0
+        k = blocksize - 1
+        for i in range(1, blocksize + 1):
+            if block[i] > block[i - 1]:
+                bit = bit | (1 << k)  # set bit at position k to 1
+            k -= 1
+        return bit
+
+    @staticmethod
+    def get_first_block_type(block, blocksize) -> int:
+        w = 0
+        k = blocksize - 2
+        for i in range(1, blocksize):
+            if block[i] > block[i - 1]:
+                w = w | (1 << k)
+            k -= 1
+        return w
+
+    @staticmethod
+    def get_pos(i, j, blocksize):
+        return sum(range(blocksize - i - 1), blocksize) + j - (blocksize - i)
+
     def _normalize_blocks(self):
-        L = deepcopy(self.depths)
+        L = list(deepcopy(self.depths))
         n = len(self.depths)
         for i in range(2, n):
-            self.depths[i] = L[i] - L[i - 1]
-            if self.depths[i] < 0:
-                self.depths[i] = 0
+            L[i] = self.depths[i] - self.depths[i - 1]
+            if L[i] < 0:
+                L[i] = 0
+        return L
 
-    def _build_look_up_tables(self):
+    def rmq_compressed_lookup(self, blocksize, max_lookup, shift=None, arr=None, lookup=None):
+        """"""
+        if lookup is None:
+            lookup = np.zeros(max_lookup, dtype='int32')
+        if shift is None:
+            shift = 0
+        if arr is None:
+            arr = self.array
+
+        for i in range(blocksize):  # O(n)
+            k = shift + self.get_pos(i, i, blocksize)
+            lookup[k] = i
+
+        for i in range(blocksize):
+            for j in range(i + 1, blocksize):
+                k = shift + self.get_pos(i, j, blocksize)
+                if arr[lookup[k - 1]] < arr[j]:
+                    lookup[k] = lookup[k - 1]
+                else:
+                    lookup[k] = j
+
+    def populate_sparse_table(self, b_count, b_size, lookup, max_lookup, T, save=True):
+        m = int(log2(b_count))
+
+        A = np.zeros(b_count, dtype='int32')
+        B = np.zeros(b_count, dtype='int32')
+
+        sparse = np.full((b_count, m + 1), inf, dtype='int32', order='F')
+
+        for i in range(0, b_count):
+            # computing the min in each block
+            pos = T[i]  # the lookup table for i starts at pos
+            min_ind = pos + self.get_pos(0, b_size - 1, b_size)
+
+            index = lookup[min_ind]  # when i = 0, j = b_size - 1; [0][b_size - 1] =
+            # 0 * b_comb + b_size - 1
+            index = i * b_size + index  # absolute index in the depths array = i * b_size + index
+
+            A[i] = self.E[index]
+            B[i] = index
+
+            # in the end, construct the sparse table
+        self.construct_sparse_table(A, sparse)
+        if save:
+            self.sparse = sparse
+            self.A = A
+            self.B = B
+
+    def build_restricted_rmq(self, save=True):
+        """"""
         n = len(self.depths)
+
         b_size = ceil(log2(n)) // 2
         b_count = ceil(n / b_size)
-        b_comb = int((b_size * (b_size + 1)) / 2)  # summation formula
-        block_types = range(2**b_size)  # e.g for block size of 2, possible blocks [0, 1, 2, 3]
 
-        self.M = np.zeros(int(b_count * b_comb), dtype='int32')
-        for i in range(b_count):
-            start = i * b_comb
-            row = 0
+        max_lookup = (b_size * (b_size + 1)) >> 1  # summation formula for block size of 2, possible blocks [0, 1, 2, 3]
 
-            # computing the minimum in each block.
-            # we
-            for j in range(b_size):  # j is the column number
-                minimum = self.depths[i * b_size]
-                for k in range(j, b_size):  # k the row number
-                    if self.depths[(i * b_size) + k - 1] == minimum:
-                        if minimum == 1:
-                            self.M[start + row] = j
-                            row += 1
-                        else:
-                            self.M[start + row] = k
-                            row += 1
-                    elif self.depths[(i * b_size) + k - 1] < minimum:
-                        self.M[start + row] = k
-                        row += 1
-                    else:
-                        self.M[start + row] = j
-                        row += 1
-        self.block_count = b_count
-        self.block_size = b_size
-        self.block_comb = b_comb
-        print("size of self.M", len(self.M))
+        size_lt = max_lookup * (1 << b_size)  # size of lookup table
 
-    def _compute_min_out_of_block(self, b_count, b_size, b_comb):
-        """assumes that lookup_tables have already been created"""
+        lookup = np.full(size_lt, -1, dtype='int32')  # the lookup table [0, 1, 2, 3 ... b_count]
 
-        m = int(log2(self.block_count))
-        self.A = np.zeros(self.block_count, dtype='int32')
-        self.B = np.zeros(self.block_count, dtype='int32')
-        sparse = np.full((self.block_count, m + 1), inf, dtype='int32', order='F')
+        T = np.zeros(b_count, dtype='int32')
 
-        for i in range(0, self.block_count):
-            # Get (0, b_size) in each block, that will stored at this location
-            idx = self.M[self.block_size - 1 + i * self.block_comb]  # in indexing, x[i, j] = i * len(one row) + j
-            idx = (i * self.block_size) + idx
+        # initialization
+        b_type = self.get_first_block_type(self.depths[: b_size], b_size)  # get b_type first element
+        self.rmq_compressed_lookup(b_size, max_lookup, 0, self.depths, lookup)
+        T[0] = b_type * max_lookup
 
-            self.A[i] = self.E[idx - 1]
-            self.B[i] = idx
+        for i in range(1, b_count):
+            b_start = i * b_size
+            b_end = min(b_start + b_size - 1, n - 1)
+            if b_end - b_start < b_size - 1:  # last block is shorter
+                new_block = np.append(self.depths[b_start - 1: b_end + 1],
+                                      [self.INFINITY] * (b_end - b_start + 1))
+                b_type = self.get_block_type(new_block, b_size)
+            else:
+                b_type = self.get_block_type(self.depths[b_start - 1: b_end + 1], b_size)
 
-        self.construct_sparse_table(array=self.A, sparse=sparse)
-        self.sparse = sparse
+            pos = b_type * max_lookup  # position of b_type in the lookup array
+            if lookup[pos] == -1:
+                self.rmq_compressed_lookup(b_size, max_lookup, pos, self.depths[b_start: b_end + 1], lookup)
+                T[i] = pos
+            else:
+                T[i] = pos
 
-    def build_restricted_rmq(self):
-        self._normalize_blocks()
-        self._build_look_up_tables()
-        self._compute_min_out_of_block(self.block_count, self.block_size, self.block_comb)
+        self.populate_sparse_table(b_count, b_size, lookup, max_lookup, T, save)
+        if save:
+            self.block_size = b_size
+            self.block_count = b_count
+            self.max_lookup = max_lookup
+            self.T = T
+            self.lookup = lookup
 
-    def _query_restricted_rmq(self, a, b):
+    def _query_restricted_rmq(self, a, b, lca=True):
         """ The overall strategy here is straight from the paper.  We look up
             the blocks that contain u and v (taking care to consider v being
             the inclusive endpoint even though the API understands it to be
@@ -105,59 +173,54 @@ class RestrictedRMQ(RMQ):
 
         i, j = self.R[a], self.R[b]
 
-        start_outer = i // self.block_size
-        end_outer = j // self.block_size
-        in_start = i % self.block_size
-        in_end = (j % self.block_size)
-        in_start_index = -1
-        in_end_index = -1
+        x = i // self.block_size  # block containing i
+        y = j // self.block_size   # block containing y
+        u = i % self.block_size  # starting position of i in block x
+        v = (j % self.block_size)  # end position of j in block y
 
-        if start_outer == end_outer:  # we are in the same block
-            left = self.block_size - in_start
-            in_start_index = start_outer * self.block_comb + (self.block_comb - ((left * (left + 1)) // 2)) + in_end
+        if x == y:   # if they are in the same block.. use lookup
+
+            ipos = self.T[x] + self.get_pos(u, v, self.block_size)
+            in_block_min = x * self.block_size + self.lookup[ipos]
+            shift = self.block_size * x
+            actual_pos = shift + in_block_min
+            if lca:
+                return self.E[actual_pos]
+            else:
+                return actual_pos
 
         else:
-            # Border case i.e. i is multiple of self.block_count e.g. 0,5,10,15
-            if in_start:  # in_start != 0
-                left = ((in_start + 1) * (2 * self.block_size - in_start)) // 2
-                in_start_index = (start_outer * self.block_comb) + left - 1
-                start_outer += 1
+            # first block minimum calculation
+            fpos = self.T[x] + self.get_pos(u, self.block_size - 1, self.block_size)
+            fmin = x * self.block_size + self.lookup[fpos]
 
-            # Border case i.e. j is 4,9,14,19 boundary of blocksize
-            if in_end != self.block_size - 1:
-                in_end_index = ((end_outer * self.block_comb) - 1) + in_end + 1  # +1/-1 because we are counting from 0
-                end_outer -= 1
+            # last block min
 
-        first_block_min = self.E[self.M[in_start_index] + ((i // self.block_size) * self.block_size)]\
-            if (in_start_index != -1) else -1
-        last_block_min = self.E[self.M[in_end_index] + ((j // self.block_size) * self.block_size)]\
-            if (in_end_index != -1) else -1
-        outer_block_min = self._query_sparse_table(start_outer, end_outer, self.A, self.sparse)\
-            if (end_outer >= start_outer) else -1
-        if self.lca:
-            minimum = inf if first_block_min == -1 else first_block_min
-            if last_block_min != -1 and last_block_min <= minimum:
-                minimum = last_block_min
-            if outer_block_min != -1 and outer_block_min <= minimum:
-                minimum = outer_block_min
-            return minimum
-        else:
-            minimum = inf if first_block_min == -1 else self.array[first_block_min]
-            minimum_index = first_block_min
-            if last_block_min != -1 and self.array[last_block_min] <= minimum:
-                minimum = self.array[last_block_min]
-                minimum_index = last_block_min
-            if outer_block_min != -1 and self.array[outer_block_min] <= minimum:
-                minimum = self.array[outer_block_min]
-                minimum_index = outer_block_min
-            return minimum_index
+            lpos = self.T[y] + self.get_pos(0, v, self.block_size)
+            lmin = y * self.block_size + self.lookup[lpos]
+
+            bmin = fmin if self.E[fmin] < self.E[lmin] else lmin
+            # super array minimum
+            if y - x > 1:  # if there are blocks between x, y
+                superpos = self.B[self._query_sparse_table(x + 1, y - 1, self.A, self.sparse)]
+                opos = bmin if self.E[bmin] < self.E[superpos] else superpos
+
+            else:
+                opos = bmin
+
+            if lca:
+                return self.E[opos]
+            else:
+                return opos
 
     def rmq(self, a, b):
         return self._query_restricted_rmq(a, b)
 
 
 if __name__ == '__main__':
-    test = [9, 1, 7, 5, 8, 12, 10, 20, 15, 18, 5, 12, 14, 98, 75, 19]
+    test = [8, 7, 2, 8, 6, 9, 4, 5]
     rq = RestrictedRMQ(test)
     rq.build_restricted_rmq()
-    print(rq.rmq(0, 14))
+    rq.norm = rq._normalize_blocks()
+    x = rq.rmq(1, 5)
+    print(x)
